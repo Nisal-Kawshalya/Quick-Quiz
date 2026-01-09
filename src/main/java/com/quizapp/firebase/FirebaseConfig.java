617cda5181a16a2ba450c5a01ceeca6719ea4e82
Nisal-Kawshalya <ict22053@std.uwu.ac.lk>
2026-01-09 20:25:09 +0530

    connect with database and creact Authentication thought role

---
+++ src/main/java/com/quizapp/firebase/FirebaseConfig.java
@@ -0,0 +1,20 @@
+package com.quizapp.firebase;
+
+public class FirebaseConfig {
+
+    // 🔐 Firebase Web API Key
+    public static final String API_KEY =
+            "AIzaSyDR7LwtyIVoYTVocB8DXJ52T8edVY4adic";
+
+    // 📦 Firebase Project ID
+    public static final String PROJECT_ID =
+            "quizapp-39b62";
+
+    // 🔑 Firebase Authentication REST base URL
+    public static final String AUTH_BASE_URL =
+            "https://identitytoolkit.googleapis.com/v1/accounts";
+
+    // 🗄 Firestore REST base URL
+    public static final String FIRESTORE_BASE_URL =
+            "https://firestore.googleapis.com/v1/projects/";
+}
